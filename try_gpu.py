ae14ad4b2e1bdf13ca3e4d96c4f7b2ce6928e65a
biu <805160804@qq.com>
2022-06-27 13:51:25 +0800

    try_gpu

---
+++ try_gpu.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+
+
+def try_gpu(i=0):  
+    """如果存在，则返回gpu(i)，否则返回cpu()"""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+def try_all_gpus():  
+    """返回所有可用的GPU，如果没有GPU，则返回[cpu(),]"""
+    devices = [torch.device(f'cuda:{i}')
+             for i in range(torch.cuda.device_count())]
+    return devices if devices else [torch.device('cpu')]
+
